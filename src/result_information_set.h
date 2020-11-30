41e4322c353cac7233bfac6d282245fc1b571aa1
tsuru_hlab_osaka <tsuru@hlab.sys.es.osaka-u.ac.jp>
2020-11-30 10:24:16 +0900

    add rotation vector to the information set style.

--- src/result_information_set.h
+++ src/result_information_set.h
@@ -12,8 +12,24 @@ public:
     {
         pos = Eigen::Vector3f(input_transform(0, 3), input_transform(1, 3), input_transform(2, 3));
         rot = input_transform.topLeftCorner(3, 3);
+
+        rotation_vector_axis = Eigen::AngleAxisf(rot).axis();
+        rotation_vector_norm = Eigen::AngleAxisf(rot).angle();
+        
+        if(rotation_vector_norm < 0.0f)
+        {
+            rotation_vector_axis = -rotation_vector_axis;
+            rotation_vector_norm = -rotation_vector_norm;
+        }
+        if(rotation_vector_norm > M_PI)
+        {
+            rotation_vector_axis = -rotation_vector_axis;
+            rotation_vector_norm = 2 * M_PI - rotation_vector_norm;
+        }
+        rotation_vector = Eigen::AngleAxisf(rotation_vector_norm, rotation_vector_axis);
+
         rpy = rot.eulerAngles(0, 1, 2);
-        Eigen::Quaternionf quat_tmp(rot);
+        Eigen::Quaternionf quat_tmp(rotation_vector);
         quat = quat_tmp;
         icp_score = input_icp_score;
     };
@@ -51,6 +67,9 @@ public:
     Eigen::Matrix3f rot;
     Eigen::Vector3f pos;
     Eigen::Vector3f rpy;
+    Eigen::AngleAxisf rotation_vector;
+    Eigen::Vector3f rotation_vector_axis;
+    float rotation_vector_norm;
     Eigen::Quaternionf quat;
     float icp_score;
 };
